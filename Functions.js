e199a93e34618119de5be4e5af274cd7ac1dba83
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 07:59:22 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,11 @@
+function onePlusAvg(x,y){
+  console.log("Done")
+  return 1 + (x+y)/2
+}
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("One plus Avarage of a and b is ", onePlusAvg(a,b));
+console.log("One plus Avarage of c and b is ", onePlusAvg(b,c));
+console.log("One plus Avarage of a and c is ", onePlusAvg(a,c));
