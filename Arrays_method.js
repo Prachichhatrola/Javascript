d96aff837c714f50d87cb12289ce60f3257dc60b
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 08:13:18 +0530

    Create Arrays_method.js

---
+++ Arrays_method.js
@@ -0,0 +1,8 @@
+//Array Method
+let num = [1, 2, 3, 34, 4]
+let b = num.toString() //b is now a string
+console.log(b)
+let c = num.join("_")
+console.log(c, typeof c)
+let r = num.shift()
+console.log(r)
