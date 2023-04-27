a8638249c72e5b33175bb3bcfd6882a6b0b73f41
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 07:58:01 +0530

    Create While_loops.js

---
+++ While_loops.js
@@ -0,0 +1,21 @@
+//while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+
+//do while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+do{
+  console.log(i)
+  i++;
+}while(i<n)
