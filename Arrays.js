b388d14ac217a11124ebd5ad27b96031c5d7c574
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 08:11:29 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+let marks_class_12 = [91, 82, 63, 84]
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log("The length of marks_class_12 is", marks_class_12.length)
