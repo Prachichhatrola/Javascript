902786e1a1574baee3569f2bc2b340d1fe6b6929
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 08:07:54 +0530

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,15 @@
+let name = "Harry"
+console.log(name.length)
+let friend = 'Prakash'
+console.log(friend)
+
+//Template literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+//Nikhil is friend of Pramod
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//Escape Sequence Characters
+let fruit = 'Bana\'na'
+console.log(fruit)
