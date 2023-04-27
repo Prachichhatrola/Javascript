ceb3c615a6fcb3202630404a6c1ce892d2520664
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 08:08:50 +0530

    Create String_method.js

---
+++ String_method.js
@@ -0,0 +1,12 @@
+let name = "Harry\""
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,4))
+// console.log(name.replace("Har","Par"))
+let friend = "Naman"
+console.log(name.concat(" is a friend of ", friend, " ok"))
+let friend2 = "     Meena    "
+console.log(friend2)
+console.log(friend2.trim())
