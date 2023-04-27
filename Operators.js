222dc8cf1e76c0551bf78454a60219a5e8ca2679
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 07:49:46 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,34 @@
+// 77;
+// "Harry"
+// false
+console.log("Operator in Js")
+// Arithmetic Operators
+let a = 45;
+let b = 4;
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a * b = ", a*b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+console.log("a++ = ", a++)
+console.log("a-- = ", a--)
+
+// Assignment Operators
+let assignment = 1;
+assignment += 5 //same as assignment = assignment + 8
+console.log(assignment)
+
+// Comparison operators
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2", comp1 == comp2)
+console.log("comp1 == comp2", comp1 != comp2)
+console.log("comp1 === comp2", comp1 === comp2)
+console.log("comp1 !== comp2", comp1 !== comp2)
+
+//Logical Operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
