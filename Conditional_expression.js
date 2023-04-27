94d88243a2438768a3718d1958585bc734e9e980
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-27 07:52:13 +0530

    Create Conditional_expression.js

---
+++ Conditional_expression.js
@@ -0,0 +1,31 @@
+let a = prompt("Hey whats your age?")
+a = Number.parseInt(a) // Converting the string to a number
+if(a<0){
+  alert("This is a Invalid age");
+}
+else if(a<9){
+  alert("You are a kid and you cannot even think of driving");
+}
+else if(a<18 && a>=9){
+  alert("You are a kid and you can think of driving after 18");
+}
+else{
+  alert("You can now drive as you are above 18");
+}
+console.log("Done")
+console.log("You can",a<18? "Not drive" : "drive")
+
+//switch case
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
