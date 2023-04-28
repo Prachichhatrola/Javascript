f067b9bdc31d41684f547491d7c5f724a2776c9a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-28 07:31:16 +0530

    Create Arrays_method2.js

---
+++ Arrays_method2.js
@@ -0,0 +1,25 @@
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+// console.log(num.length)
+// delete num[0]
+// console.log(num.length)
+
+// let newArray = num.concat(num_more)
+// console.log(newArray)
+
+//sort method
+// let compare = (a, b)=>{
+//   return a - b
+// }
+
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// num.sort(compare)
+// console.log(num)
+
+// splice and slice
+let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// num.splice(2, 4, 1022, 1023, 1024, 1025)
+// console.log(num)
+
+let newNum = num.slice(3, 5)
+console.log(newNum)
