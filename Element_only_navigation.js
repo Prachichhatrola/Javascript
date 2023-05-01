6b2f5a11304b82fddaa491a00719c18f85610018
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-01 17:44:48 +0530

    Create Element_only_navigation.js

---
+++ Element_only_navigation.js
@@ -0,0 +1,8 @@
+const changeBgRed = () => {
+  document.body.firstElementChild.style.backgroung = "red"
+}
+
+
+let b = document.body
+console.log("First child of b is: ", b.firstChild)
+console.log("First child of b is: ", b.firstElementChild)
